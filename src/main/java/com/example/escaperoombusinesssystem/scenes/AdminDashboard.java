8e348896fbf3d883f6282392df69ebdad3018661
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2025-04-30 04:11:10 +0300

    added basic admin dashboard class

---
+++ src/main/java/com/example/escaperoombusinesssystem/scenes/AdminDashboard.java
@@ -0,0 +1,11 @@
+package com.example.escaperoombusinesssystem.scenes;
+
+import javafx.stage.Stage;
+
+public class AdminDashboard {
+    Stage stage;
+
+    AdminDashboard(Stage stage) {
+        this.stage = stage;
+    }
+}
