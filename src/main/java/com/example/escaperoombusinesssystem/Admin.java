7c4c142bfb9c9eeeadc43f46337f900f22320204
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2025-04-30 13:40:40 +0300

    Implemented Signup ui

---
+++ src/main/java/com/example/escaperoombusinesssystem/Admin.java
@@ -0,0 +1,14 @@
+package com.example.escaperoombusinesssystem;
+
+public class Admin extends User {
+
+    public Admin(String username) {
+        super(username , "com.example.escaperoombusinesssystem.Admin");
+    }
+
+   //public void accessDashboard(){}
+   //public com.example.escaperoombusinesssystem.Report generateReport(){}
+   // public void addRoom(){}
+
+
+}
