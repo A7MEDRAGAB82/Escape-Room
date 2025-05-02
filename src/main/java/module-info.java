f951742b6a0b45b8e96d25b34048118fe09aeb60
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2025-05-02 08:28:31 +0300

    removed sign up db implemntation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,8 @@ module com.example.escaperoombusinesssystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-<<<<<<< HEAD
     requires io.github.cdimascio.dotenv.java;
-=======
     requires jbcrypt;
->>>>>>> 6ba90a3ec4477c740073692730067ae1eab6564a
 
 
     opens com.example.escaperoombusinesssystem to javafx.fxml;
